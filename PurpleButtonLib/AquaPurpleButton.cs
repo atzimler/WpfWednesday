dc762446a4c3b5093d8a5b8317350c62fdbbc45c
Attila Tamas Zimler <attila.tamas.zimler@icloud.com>
2017-04-08 17:05:24 +1000

    Separating the purple button and creating an AquaPurpleButton in code.

---
+++ PurpleButtonLib/AquaPurpleButton.cs
@@ -0,0 +1,13 @@
+﻿using System.Windows.Media;
+
+namespace PurpleButtonLib
+{
+    public class AquaPurpleButton : PurpleButton
+    {
+        public AquaPurpleButton()
+        {
+            Background = Brushes.Aqua;
+            BorderBrush = Brushes.DarkCyan;
+        }
+    }
+}
