219fe266eb25143b058c42f47a967968039f9131
Attila Tamas Zimler <attila.tamas.zimler@icloud.com>
2017-04-09 14:35:15 +1000

    Starting version of Z05_Deriving - before I get again frustrated and have to restart and because of the mess have to remove even the part that I know is ok.

---
+++ Z05_Deriving/PurpleButtonLib/PurpleButton.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace PurpleButtonLib
+{
+    /// <summary>
+    /// Interaction logic for PurpleButton.xaml
+    /// </summary>
+    public partial class PurpleButton
+    {
+        public PurpleButton()
+        {
+            InitializeComponent();
+        }
+    }
+}
