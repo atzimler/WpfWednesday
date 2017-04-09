7b6f6c0c841893d16f71d33ffa3072756453face
Attila Tamas Zimler <attila.tamas.zimler@icloud.com>
2017-04-09 11:29:22 +1000

    Code steps in the related article.

---
+++ Z04_Deriving/WpfApplication/AquaPurpleButton.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows.Media;
+using PurpleButtonLib;
+
+namespace WpfApplication
+{
+    public class AquaPurpleButton : PurpleButton
+    {
+        public AquaPurpleButton()
+        {
+            Background = Brushes.Aqua;
+            BorderBrush = Brushes.DarkCyan;
+        }
+    }
+}
