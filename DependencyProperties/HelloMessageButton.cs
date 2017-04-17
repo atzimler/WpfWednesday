ec1d760693fc06d03b44c044c3292354c45b3c48
Attila Tamas Zimler <attila.tamas.zimler@icloud.com>
2017-04-17 12:39:42 +1000

    Example for Dependency Properties.

---
+++ DependencyProperties/HelloMessageButton.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace DependencyProperties
+{
+    public class HelloMessageButton : Button
+    {
+        public static DependencyProperty MessageProperty = DependencyProperty.Register(nameof(Message), typeof(string), typeof(HelloMessageButton), new PropertyMetadata(null));
+
+        public string Message
+        {
+            get { return (string)GetValue(MessageProperty); }
+            set { SetValue(MessageProperty, value); }
+        }
+
+        public HelloMessageButton()
+        {
+            Click += (o, e) => MessageBox.Show(Message);
+        }
+    }
+}
