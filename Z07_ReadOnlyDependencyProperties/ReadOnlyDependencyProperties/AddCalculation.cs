bfff21abbbaf3ea94aa4de310ccd2fd63a88699a
Attila Tamas Zimler <attila.tamas.zimler@icloud.com>
2017-04-25 20:56:19 +1000

    WPF Wednesday article.

---
+++ Z07_ReadOnlyDependencyProperties/ReadOnlyDependencyProperties/AddCalculation.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Windows;
+
+namespace ReadOnlyDependencyProperties
+{
+    public class AddCalculation : DependencyObject
+    {
+        private static readonly DependencyPropertyKey ResultPropertyKey =
+            DependencyProperty.RegisterReadOnly(nameof(Result), typeof(int), typeof(AddCalculation), new PropertyMetadata(0));
+        public static readonly DependencyProperty ResultProperty = ResultPropertyKey.DependencyProperty;
+        public int Result
+        {
+            get { return (int)GetValue(ResultProperty); }
+        }
+
+        public static readonly DependencyProperty AProperty =
+            DependencyProperty.Register(nameof(A), typeof(int), typeof(AddCalculation), new PropertyMetadata(0, AddNumbers));
+        public int A
+        {
+            get { return (int)GetValue(AProperty); }
+            set { SetValue(AProperty, value); }
+        }
+
+        public static readonly DependencyProperty BProperty =
+            DependencyProperty.Register(nameof(B), typeof(int), typeof(AddCalculation), new PropertyMetadata(0, AddNumbers));
+        public int B
+        {
+            get { return (int)GetValue(BProperty); }
+            set { SetValue(BProperty, value); }
+        }
+
+        private static void AddNumbers(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            var ac = (AddCalculation)d;
+            ac.SetValue(ResultPropertyKey, ac.A + ac.B);
+        }
+
+    }
+}
---
+++ Z07_ReadOnlyDependencyProperties/ReadOnlyDependencyProperties/ReadOnlyDependencyPropertiesShould.cs
@@ -0,0 +1,38 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace ReadOnlyDependencyProperties
+{
+    [TestFixture]
+    public class ReadOnlyDependencyPropertiesShould
+    {
+        [Test]
+        public void BeReadableByAnyone()
+        {
+            var ac = new AddCalculation
+            {
+                A = 42
+            };
+            Assert.AreEqual(42, ac.A);
+        }
+
+        [Test]
+        public void BeWritableByDeclaringClass()
+        {
+            var ac = new AddCalculation
+            {
+                A = 13,
+                B = 42
+            };
+            Assert.AreEqual(55, ac.Result);
+        }
+
+        [Test]
+        public void BeNonWritableByOutsideClasses()
+        {
+            var ac = new AddCalculation();
+            var ex = Assert.Throws<InvalidOperationException>(() => ac.SetValue(AddCalculation.ResultProperty, 13));
+            Assert.AreEqual("'Result' property was registered as read-only and cannot be modified without an authorization key.", ex.Message);
+        }
+    }
+}
