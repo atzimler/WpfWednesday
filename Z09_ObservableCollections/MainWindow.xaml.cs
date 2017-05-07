9addcd82d600c91814372da3b8443813b992aa3c
Attila Tamas Zimler <attila.tamas.zimler@icloud.com>
2017-05-07 17:28:44 +1000

    Adding example code for demonstrating ObservableCollections.

---
+++ Z09_ObservableCollections/MainWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Windows;
+
+namespace Z09_ObservableCollections
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public List<int> ListItems { get; } = new List<int> { 1 };
+        public ObservableCollection<int> ObservableCollectionItems { get; } = new ObservableCollection<int> { 1 };
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            DataContext = this;
+            ListButton.Click += (sender, args) => 
+                ListItems.Add(ListItems[ListItems.Count - 1] + 1);
+            ObservableCollectionButton.Click +=
+                (sender, args) => ObservableCollectionItems.Add(
+                    ObservableCollectionItems[ObservableCollectionItems.Count - 1] + 1);
+        }
+    }
+}
