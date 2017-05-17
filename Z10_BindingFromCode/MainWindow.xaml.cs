30390b890f414bdfaef83d734b6fb789c993e69d
Attila Tamas Zimler <attila.tamas.zimler@icloud.com>
2017-05-17 21:36:30 +1000

    Today's article.

---
+++ Z10_BindingFromCode/MainWindow.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+
+namespace Z10_BindingFromCode
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
+    {
+        public static DependencyProperty LabelDataProperty = DependencyProperty.Register(nameof(LabelData), typeof(string), typeof(MainWindow), new PropertyMetadata("Old Label Value"));
+        public string LabelData
+        {
+            get { return (string)GetValue(LabelDataProperty); }
+            set { SetValue(LabelDataProperty, value); }
+        }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            var binding = new Binding(nameof(LabelData)) { Source = this, Mode = BindingMode.OneWay };
+            BindingOperations.SetBinding(UpdatedLabel, TextBlock.TextProperty, binding);
+
+            UpdateLabelButton.Click += (s, a) => { LabelData = "New Label Value"; };
+        }
+    }
+}
