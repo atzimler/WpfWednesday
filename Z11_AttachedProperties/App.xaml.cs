26be932f5d81e7ac77a435b4bba8a916090e07cc
Attila Tamas Zimler <attila.tamas.zimler@icloud.com>
2017-05-24 21:48:59 +1000

    AttachedProperties.

---
+++ Z11_AttachedProperties/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Z11_AttachedProperties
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
---
+++ Z11_AttachedProperties/LeftRightLayoutControl.cs
@@ -0,0 +1,78 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace Z11_AttachedProperties
+{
+    public class LeftRightLayoutControl : StackPanel
+    {
+        private Grid _grid;
+        private StackPanel _left;
+        private StackPanel _right;
+
+        public enum Side
+        {
+            None,
+            Left,
+            Right
+        };
+        public static readonly DependencyProperty SideProperty = DependencyProperty.RegisterAttached(nameof(Side), typeof(Side), typeof(LeftRightLayoutControl), new PropertyMetadata(Side.None, UpdateSide));
+
+        private static void UpdateSide(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            var obj = (FrameworkElement)d;
+            var parent = LogicalTreeHelper.GetParent(obj);
+            while (parent != null && parent.GetType() != typeof(LeftRightLayoutControl))
+            {
+                parent = LogicalTreeHelper.GetParent(parent);
+            }
+
+            var layoutControl = (LeftRightLayoutControl)parent;
+            ((StackPanel)obj.Parent).Children.Remove(obj);
+
+            var value = (Side)e.NewValue;
+            if (value == Side.Left)
+            {
+                layoutControl._left.Children.Add(obj);
+            }
+            else if (value == Side.Right)
+            {
+                layoutControl._right.Children.Add(obj);
+            }
+            else
+            {
+                layoutControl.Children.Add(obj);
+            }
+        }
+
+        public static Side GetSide(UIElement target)
+        {
+            return (Side)target.GetValue(SideProperty);
+        }
+
+        public static void SetSide(UIElement target, Side value)
+        {
+            target.SetValue(SideProperty, value);
+        }
+
+        public LeftRightLayoutControl()
+        {
+            _grid = new Grid();
+            _grid.RowDefinitions.Add(new RowDefinition { Height = new GridLength(1, GridUnitType.Star) });
+            _grid.ColumnDefinitions.Add(new ColumnDefinition { Width = new GridLength(1, GridUnitType.Star) });
+            _grid.ColumnDefinitions.Add(new ColumnDefinition { Width = new GridLength(1, GridUnitType.Star) });
+
+            _left = new StackPanel();
+            Grid.SetRow(_left, 0);
+            Grid.SetColumn(_left, 0);
+            _grid.Children.Add(_left);
+
+            _right = new StackPanel();
+            Grid.SetRow(_right, 0);
+            Grid.SetColumn(_right, 1);
+            _grid.Children.Add(_right);
+
+            Children.Add(_grid);
+        }
+
+    }
+}
